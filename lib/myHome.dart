149bc2d12fce7fcefa3c635558f731db81212d67
Ambuj Kumar <itsambuja@gmail.com>
2019-04-17 17:19:55 +0530

    userType added as constructor

--- lib/myHome.dart
+++ lib/myHome.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import './nav/bottomNavigation.dart';
 class MyHomePage extends StatefulWidget {
   final String userId;
-  MyHomePage({Key key,this.userId});
+  final String userType;
+  MyHomePage({Key key,this.userId,this.userType});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -12,6 +13,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     //return Tabs(userId: widget.userId,);
-    return BottomNavigation(userId: widget.userId);
+    return BottomNavigation(userType: widget.userType, userId: widget.userId);
   }
 }
