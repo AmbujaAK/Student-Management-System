4016b994934afa085e7387e9a4dc867c20a1d20d
Ambuj Kumar <itsambuja@gmail.com>
2019-04-19 16:16:46 +0530

    a folder will be created on installing this app

--- lib/myHome.dart
+++ lib/myHome.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 import './nav/bottomNavigation.dart';
 class MyHomePage extends StatefulWidget {
@@ -10,6 +15,29 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 class _MyHomePageState extends State<MyHomePage> {
+
+  createDir() async {
+    Directory baseDir = await getExternalStorageDirectory(); //only for Android
+    
+    String dirToBeCreated = "Flutter";
+    String finalDir = join(baseDir.path, dirToBeCreated);
+    
+    var dir = Directory(finalDir);
+    bool dirExists = await dir.exists();
+    if(!dirExists){
+      dir.create(/*recursive=true*/); //pass recursive as true if directory is recursive
+    }else{
+      print('created :: $dir');
+    }
+    //Now you can use this directory for saving file, etc.
+    //In case you are using external storage, make sure you have storage permissions.
+  }
+
+  @override
+  void initState() {
+    createDir();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     //return Tabs(userId: widget.userId,);
